9e82e744817be19aa89ac5c9854b69829d5238ab
Nickeniklas <savonhen@arcada.fi>
2024-09-26 16:22:26 +0300

    friends fetching app

---
+++ niklas.assignment3/src/main/java/com/arcada/devops/niklas/assignment3/App.java
@@ -0,0 +1,48 @@
+package com.arcada.devops.niklas.assignment3;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+    	
+    	Scanner input = new Scanner(System.in);
+    	
+    	System.out.println("Search for a persons friends list(try juha or elias): ");
+    	String userInput = input.nextLine(); 
+         String[] friends = getFriendNames(userInput);
+         
+         // Print the friends' names
+         for (String friend : friends) {
+             System.out.println(friend);
+         }
+     }
+
+     public static String[] getFriendNames(String name) {
+    	 System.out.println(name);
+    	 String[] juhaFriends = {
+    	            "Kalle", "Ville", "Jukka", "Spurgu", 
+    	            "Ukko", "Pelle", "Matti", "Sami", 
+    	            "Antti", "Leena", "Riikka", "Kaisa", 
+    	            "Timo", "Jani", "Eero", "Elias"
+    	        };
+    	 String[] eliasFriends = {
+    	            "Aino", "Kyllikki", "Jouko", "Hannes", 
+    	            "Eerik", "Antero", "Eeva", "Ilona", 
+    	            "Seppo", "Rauni", "Aulikki", "Aarre", 
+    	            "Martti", "Sirkka", "Terttu", "Ulla"
+    	        };
+    	 if(name.equals("juha")) {
+    		 return juhaFriends;
+    	 }
+    	 if(name.equals("elias")) {
+    		 return eliasFriends;
+    	 }
+         
+    	 return new String[]{"No such user..."};
+         
+     }
+    
+}
